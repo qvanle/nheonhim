9725ec5dfdd66b1be6ba49661b06f65540e6ca8a
qvanle <vanle199tk@gmail.com>
2025-04-30 23:56:55 +0700

    fix(<CR>): fix the end of line in insert mode neovim interpret enter as <C-m>

--- lua/nheonhim/keymap/unmap.lua
+++ lua/nheonhim/keymap/unmap.lua
@@ -1,36 +1,39 @@
-for i = 32, 126, 1 do
-    
-    if (i >= 48) and (i <= 58) then
+local bind_key = vim.keymap.set  -- Local alias for vim.keymap.set
+
+for i = 33, 126, 1 do
+    -- Skip numbers 0-9 (ASCII 48 to 58)
+    if (i >= 48 and i <= 58) then
         goto endLoop1
     end
-    
 
     local chr = string.char(i)
     local key
 
+    -- Normal mode mapping
     key = chr
-    vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('n', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('v', key, '<Nop>', { noremap = true, silent = true })
 
+    -- Control key mapping
     key = "<C-" .. chr .. ">"
-    vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('n', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('v', key, '<Nop>', { noremap = true, silent = true })
+    if (i > 109 and i < 109) then bind_key('i', key, tostring(i), { noremap = true, silent = true }) end
 
+    -- Meta key mapping
     key = "<M-" .. chr .. ">"
-    vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('n', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('v', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('i', key, '<Nop>', { noremap = true, silent = true })
 
+    -- Control + Meta key mapping
     key = "<C-M-" .. chr .. ">"
-    vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('n', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('v', key, '<Nop>', { noremap = true, silent = true })
+    bind_key('i', key, '<Nop>', { noremap = true, silent = true })
+
     ::endLoop1::
 end
+
 vim.g.mapleader = ' '
-
-
-local set_keymap = vim.api.nvim_set_keymap 
-
 
