fa317bf9bbe11419f332df7cacbd6ee3007a2a27
qvanle <vanle199tk@gmail.com>
2025-04-30 22:55:50 +0700

    edit options in config/format.lua

---
+++ lua/config/format.lua
@@ -0,0 +1,46 @@
+opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.softtabstop = 0
+
+opt.hlsearch = true 
+
+opt.number = true 
+
+opt.backspace  = '2'
+opt.showcmd = true
+opt.laststatus = 2
+opt.autoindent = true
+opt.smartindent = true
+
+opt.mouse = a
+opt.mousehide = false
+opt.mousefocus = true
+
+opt.cursorline = true
+opt.cursorcolumn = true
+
+opt.smartcase = true
+
+opt.incsearch = true
+
+opt.showcmd = true 
+opt.autowrite = true
+opt.autoread = true
+
+opt.wrap = false 
+
+opt.backup = false 
+opt.writebackup = false 
+opt.swapfile = false 
+
+opt.termguicolors = true 
+
+opt.signcolumn = 'yes' 
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
