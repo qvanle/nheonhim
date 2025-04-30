61ad571a6b11b7abb2f543ee7afdb3e2025e2f6b
qvanle <vanle199tk@gmail.com>
2025-04-30 23:37:37 +0700

    remove mock/unmap and add nheonhim/unmap

--- lua/mock-setup/config/mapping/mode.lua
+++ lua/mock-setup/config/mapping/mode.lua
@@ -3,3 +3,7 @@ set_keymap('n', 'i', '<Cmd>lua vim.api.nvim_feedkeys("i", "n", false)<CR>', { no
 set_keymap('n', 'a', '<Cmd>lua vim.api.nvim_feedkeys("a", "n", false)<CR>', { noremap = true, silent = true })
 set_keymap('n', '<ESC>', '<Cmd>lua vim.api.nvim_feedkeys("<esc>", "n", false<CR>)', {noremap = true, silent = true } )
 set_keymap('n', 'ZZ', '<cmd>normal! ZZ<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<ESC>', function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n', false)
+end)
+
--- lua/nheonhim/keymap/unmap.lua
+++ lua/nheonhim/keymap/unmap.lua
@@ -1,22 +1,9 @@
 for i = 32, 126, 1 do
+    
     if (i >= 48) and (i <= 58) then
         goto endLoop1
     end
-    if (i == 65) then
-        goto endLoop1
-    end
-    if(i == 94) then
-        goto endLoop1
-    end
-
-    if(i == 95) then
-        goto endLoop1
-    end
-
-    if(i == 96) then
-        goto endLoop1
-    end
-
+    
 
     local chr = string.char(i)
     local key
@@ -28,19 +15,22 @@ for i = 32, 126, 1 do
     key = "<C-" .. chr .. ">"
     vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
 
     key = "<M-" .. chr .. ">"
     vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
 
     key = "<C-M-" .. chr .. ">"
     vim.api.nvim_set_keymap('n', key, '<Nop>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', key, '<Nop>', { noremap = true, silent = true })
-
+    vim.api.nvim_set_keymap('i', key, '<Nop>', { noremap = true, silent = true })
     ::endLoop1::
 end
 vim.g.mapleader = ' '
 
 
 local set_keymap = vim.api.nvim_set_keymap 
+
 
