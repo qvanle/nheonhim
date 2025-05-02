a3a07f53cfcc11c4f48f60a8375ffacdbbe6b606
qvanle <vanle199tk@gmail.com>
2025-05-02 16:35:09 +0700

    feat:add <leader>BB for using formatter

---
+++ lua/nheonhim/keymap/plugins/formatter.lua
@@ -0,0 +1,12 @@
+local bind_key = vim.keymap.set
+local feedkeys = vim.api.nvim_feedkeys
+local termcodes = vim.api.nvim_replace_termcodes
+local opts = { noremap = true, silent = true }
+
+local function key(k)
+	return function()
+		feedkeys(termcodes(k, true, false, true), "n", false)
+	end
+end
+
+bind_key("n", "<leader>BB", key("<cmd>FormatWrite<CR>"), opts)
