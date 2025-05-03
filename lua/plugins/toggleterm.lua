792dfb74613af30f0fb849d206810cb09c14b805
qvanle <vanle199tk@gmail.com>
2025-05-03 12:49:53 +0700

    feat:add border to terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,13 +1,23 @@
 local M = {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-        require("toggleterm").setup({
-            direction = "horizontal",
-            shell = "/bin/zsh -l",  -- Forces zsh to run as a login shell, which sources ~/.zshrc
-        })
-    end
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			direction = "float",
+			shell = "/bin/zsh -l", -- Forces zsh to run as a login shell, which sources ~/.zshrc
+			float_opts = {
+				border = "double", -- 'single' | 'double' | 'shadow' | 'curved'
+				winblend = 3,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+				width = 110,
+				height = 10,
+				row = vim.o.lines + 101, -- subtract 21 to account for height + 1-based row index
+			},
+		})
+	end,
 }
 
 return M
-
